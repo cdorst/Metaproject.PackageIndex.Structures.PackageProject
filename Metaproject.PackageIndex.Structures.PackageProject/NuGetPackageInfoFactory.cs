3c647b472455b122a1b04ca3030ac94988b8d55c
Christopher Dorst <chris@dorst.email>
2018-01-16 21:52:19 -0600

    Add PackageCsproj

---
+++ Metaproject.PackageIndex.Structures.PackageProject/NuGetPackageInfoFactory.cs
@@ -0,0 +1,20 @@
+﻿using DevOps.Primitives.VisualStudio.Projects.Helpers.DotNetCore;
+
+namespace Metaproject.PackageIndex.Structures.PackageProject
+{
+    internal static class NuGetPackageInfoFactory
+    {
+        private const string Author = "cdorst";
+        private const string IdPrefix = "CDorst.";
+
+        public static NuGetPackageInfo PackageInfo(string name, string description, string version)
+            => new NuGetPackageInfo
+            {
+                Authors = Author,
+                Description = description,
+                GeneratePackageOnBuild = true,
+                PackageId = $"{IdPrefix}{name}",
+                Version = version
+            };
+    }
+}
---
+++ Metaproject.PackageIndex.Structures.PackageProject/PackageCsproj.cs
@@ -0,0 +1,30 @@
+﻿using DevOps.Primitives.NuGet;
+using DevOps.Primitives.VisualStudio.Projects;
+using System.Collections.Generic;
+using static DevOps.Primitives.VisualStudio.Projects.Helpers.DotNetCore.DotNetCoreProjects;
+using static Metaproject.PackageIndex.Structures.PackageProject.NuGetPackageInfoFactory;
+
+namespace Metaproject.PackageIndex.Structures.PackageProject
+{
+    public class PackageCsproj
+    {
+        public PackageCsproj(string name, string description, string version, List<NuGetReference> packageReferences)
+        {
+            Description = description;
+            Name = name;
+            PackageReferences = packageReferences;
+            Version = version;
+        }
+
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public string Version { get; set; }
+
+        public List<NuGetReference> PackageReferences { get; set; }
+
+        public Project GetProject()
+            => Create(Name,
+                PackageReferences,
+                nuGetPackageInfo: PackageInfo(Name, Description, Version));
+    }
+}
