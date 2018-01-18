dfd47307ec0383368eac8c0d0816e4af630eded8
Christopher Dorst <chris@dorst.email>
2018-01-18 03:02:39 -0600

    Add PackageId property

--- Metaproject.PackageIndex.Structures.PackageProject/PackageCsproj.cs
+++ Metaproject.PackageIndex.Structures.PackageProject/PackageCsproj.cs
@@ -8,16 +8,18 @@ namespace Metaproject.PackageIndex.Structures.PackageProject
 {
     public class PackageCsproj
     {
-        public PackageCsproj(string name, string description, string version, List<NuGetReference> packageReferences)
+        public PackageCsproj(string name, string description, string packageId, string version, List<NuGetReference> packageReferences)
         {
             Description = description;
             Name = name;
+            PackageId = packageId;
             PackageReferences = packageReferences;
             Version = version;
         }
 
         public string Description { get; set; }
         public string Name { get; set; }
+        public string PackageId { get; set; }
         public string Version { get; set; }
 
         public List<NuGetReference> PackageReferences { get; set; }
@@ -25,6 +27,6 @@ namespace Metaproject.PackageIndex.Structures.PackageProject
         public Project GetProject()
             => Create(Name,
                 PackageReferences,
-                nuGetPackageInfo: PackageInfo(Name, Description, Version));
+                nuGetPackageInfo: PackageInfo(PackageId, Description, Version));
     }
 }
